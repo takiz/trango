6946ed96f7fe7225de733a67100ba20f7e601cb9
takiz <koechto2@yandex.ru>
2020-05-23 20:41:37 +0500

    LabelColor

--- trango.go
+++ trango.go
@@ -576,8 +576,8 @@ func NewInputFieldPrim(label string) *tview.InputField {
 	inp := tview.NewInputField().
 		SetLabel(label).
 		SetFieldWidth(50).SetFieldTextColor(tcell.Color221).
-		SetFieldBackgroundColor(tcell.Color25)
-	// SetLabelColor(tcell.Color221)
+		SetFieldBackgroundColor(tcell.Color25).
+		SetLabelColor(tcell.ColorDefault)
 	inp.SetBackgroundColor(tcell.ColorDefault)
 	return inp
 }
